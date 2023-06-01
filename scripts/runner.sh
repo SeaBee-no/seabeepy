37d811b5f3ba48a365bd1aeb1418a132bcbfb9d1
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-06-01 15:18:40 +0200

    update to flights runner

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mc alias set seabee-minio https://storage.seabee.sigma2.no $MINIO_ACCESS_ID $MINIO_SECRET_KEY
-jupyter nbconvert --execute  --allow-errors --to notebook --inplace ./seabirds_runner.ipynb
-mc cp ./seabirds_runner.ipynb seabee-minio/notebook-logs/seabirds_runner-$( date '+%H' ).ipynb
+jupyter nbconvert --execute  --allow-errors --to notebook --inplace ./flights_runner.ipynb
+mc cp ./flights_runner.ipynb seabee-minio/notebook-logs/flights_runner-$( date '+%H' ).ipynb
