48895edc6e8918bb34e8b3559ad38d59271923bf
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-06-01 13:25:20 +0200

    add notebooks

---
+++ notebooks/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    MINIO_ACCESS_ID: str
+    MINIO_SECRET_KEY: str
+    GEOSERVER_USER: str
+    GEOSERVER_PASSWORD: str
+    GEONODE_USER:str
+    GEONODE_PASSWORD: str
+
+    class Config:
+        case_sensitive = False
+        env_file = ".env"
+
+
+SETTINGS = Settings()
