079213af6dfc82958aeaa33c096ea95217c440e0
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-06-02 22:12:57 +0200

    make sure lib is available

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -3,4 +3,4 @@ echo "Flight take-off"
 mc alias set seabee-minio https://storage.seabee.sigma2.no $MINIO_ACCESS_ID $MINIO_SECRET_KEY
 jupyter nbconvert --execute  --allow-errors --to notebook --inplace ./flights_runner.ipynb
 mc cp ./flights_runner.ipynb seabee-minio/notebook-logs/flights_runner-$( date '+%H' ).ipynb
-echo "Landing"
+echo "Flight landing"
